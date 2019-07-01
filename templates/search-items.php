6987bf4c5ed3bf0c0a00ac25d664c7abd34e3f60
TruongTuyen <hieudieuluong@gmail.com>
2019-07-01 08:22:38 +0700

    Update thumb image size in search result

--- templates/search-items.php
+++ templates/search-items.php
@@ -15,10 +15,11 @@ if ( $maybe_display_thumb && has_post_thumbnail() ) {
 ?>
 <div class="live-search-item <?php echo esc_attr( implode( ' ', $item_extra_class ) ); ?>" data-posttype="<?php echo esc_attr( $posttype ); ?>" data-posttype_label="<?php echo esc_attr( $posttype_label ); ?>" data-href="<?php the_permalink(); ?>">
 	<?php do_action( 'press_search_before_live_item_thumbnail', get_the_ID() ); ?>
-	<?php if ( $maybe_display_thumb && get_the_post_thumbnail_url() ) { ?>
+	<?php
+	$post_thumb_url = get_the_post_thumbnail_url( get_the_ID(), array( 100, 100 ) );
+	if ( $maybe_display_thumb && ! empty( $post_thumb_url ) ) { ?>
 		<div class="item-thumb">
 			<?php
-			$post_thumb_url = get_the_post_thumbnail_url();
 			$post_thumb_url = apply_filters( 'press_search_live_search_item_post_thumb_url', $post_thumb_url, $posttype, get_the_ID() );
 			if ( ! empty( $post_thumb_url ) ) {
 				echo sprintf( '<a href="%s" style="%s" class="item-thumb-link"></a>', get_the_permalink(), 'background-image: url(' . $post_thumb_url . ');' );
